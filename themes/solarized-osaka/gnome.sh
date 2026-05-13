9ee5467dd43df0d830dfae5447c09416e32a4e83
Bruno Lima <brunoluislima19@gmail.com>
2026-05-13 10:45:55 -0300

    feat(solarized-osaka): change gnome accent color to viridian

--- themes/solarized-osaka/gnome.sh
+++ themes/solarized-osaka/gnome.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-OMAKGNOME_THEME_COLOR="blue"
+OMAKGNOME_THEME_COLOR="viridian"
 OMAKGNOME_THEME_BACKGROUND="solarized-osaka/background.jpg"
 source $OMAKGNOME_PATH/themes/set-gnome-theme.sh
