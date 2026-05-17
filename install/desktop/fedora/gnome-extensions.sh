b56a991cd0ae74b2bb3c26d703c7f344f779b98f
Bruno Lima <brunoluislima19@gmail.com>
2026-05-16 23:58:24 -0300

    feat: add AlphabeticalAppGrid extension install (Ubuntu/Fedora)

--- install/desktop/fedora/gnome-extensions.sh
+++ install/desktop/fedora/gnome-extensions.sh
@@ -5,6 +5,10 @@ pipx ensurepath
 pipx install gnome-extensions-cli --system-site-packages
 
 gext install blur-my-shell@aunetx
+gext install AlphabeticalAppGrid@stuarthayhurst
 
 sudo cp ~/.local/share/gnome-shell/extensions/blur-my-shell\@aunetx/schemas/org.gnome.shell.extensions.blur-my-shell.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/AlphabeticalAppGrid\@stuarthayhurst/schemas/org.gnome.shell.extensions.AlphabeticalAppGrid.gschema.xml /usr/share/glib-2.0/schemas/
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
+
+gsettings set org.gnome.shell.extensions.alphabetical-app-grid folder-order-position 'end'
